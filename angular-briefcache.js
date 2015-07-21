1b3ad8e4b02a13d2ddddf5f0709a663fde87c9c0
Eric Heikes <eric.heikes@banno.com>
2015-07-21 10:26:23 -0500

    Convert to an Angular provider so it can be configured pre-bootstrap.

--- angular-briefcache.js
+++ angular-briefcache.js
@@ -1,9 +1,13 @@
-angular.module('banno.briefCache', ['angular-cache']).factory('briefCache', ['CacheFactory', function(CacheFactory) {
+angular.module('banno.briefCache', ['angular-cache']).provider('briefCache', function() {
 	'use strict';
-	/* jshint newcap:false */
-	return CacheFactory.get('briefCache') || CacheFactory('briefCache', {
-		maxAge: 10 * 1000, // expire after 10 secs
-		cacheFlushInterval: 60 * 60 * 1000, // clear itself every hour
-		deleteOnExpire: 'passive' // delete as they are requested
-	});
-}]);
+	return {
+		$get: ['CacheFactory', function(CacheFactory) {
+			/* jshint newcap:false */
+			return CacheFactory.get('briefCache') || CacheFactory('briefCache', {
+				maxAge: 10 * 1000, // expire after 10 secs
+				cacheFlushInterval: 60 * 60 * 1000, // clear itself every hour
+				deleteOnExpire: 'passive' // delete as they are requested
+			});
+		}]
+	};
+});
